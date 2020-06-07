753fb945767f46c1e0eabf5354c1999d8ed29762
joshurbandavis <joshurbandavis@gmail.com>
2020-06-07 19:46:51 -0400

    Add files via upload

--- mySketch.js
+++ mySketch.js
@@ -13,7 +13,7 @@ let m, bf1, bf2;
 let cx1, cy1;
 
 function setup() {
-  createCanvas(1200, 1500);
+  createCanvas(1160, 1500);
   //createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   background(255);
@@ -30,15 +30,32 @@ function setup() {
 
 function draw() {
 
-  //
-  //vessel 1
-  //
+  //vessel 1 (center)
+  vessel(width * .3, height * .37, 2, 10, 5, 5);
+  //vessel 2 (left lower)
+  vessel(width * .3, height * .6, 3, 28, 5, 4);
+  //vessel 3 (lower right)
+  vessel(width * .39, height * .77, 1.75, 28, 5, 6);
+  //vessel 4 (far right)
+  vessel(width * .22, height * .23, 1.35, 28, 5, 5);
+  //vessle 5 (far left)
+  vessel(width * .26, height * .13, 4.25, 10, 5, 4);
+
+}
+
+// function keyPressed() {
+//     save('pix.jpg');
+// }
+
+function vessel(radius, y_input, position, strokeWeightOuter, strokeWeightInner, freq) {
 
   randomSeed(seed);
 
   let d = int(width / 30 + 50);
-  let x = width / 2, y = 600;
-  let m = 350 + sin(frameCount * 4) * 50;
+  let x = width / position, y = y_input;
+  let m = radius + sin(frameCount * freq) * 50;
+
+  //let m = m/width;
 
   push();
   translate(x, y);
@@ -46,10 +63,10 @@ function draw() {
   push();
   translate(0, 0);
   //noFill();  strokeWeight(10);  stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
-  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(10); stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
+  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(strokeWeightOuter); stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
   ellipse(0, 0, m, m / 2);
   //noFill();  strokeWeight(5);  stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
-  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(5); stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
+  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(strokeWeightInner); stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
   //ellipse(0, 0, m, m/2);
   pop();
 
@@ -62,170 +79,12 @@ function draw() {
 
   // effect();
   // drawWindow();
-  noStroke();
-  fill(90);
-  rect(0, height - 84, width, height);
-  fill(50);
-  rect(0, height - 64, width, height);
-
-  //
-  //vessel 2
-  //
-
-  randomSeed(seed);
-
-  let d2 = int(width / 30 + 50);
-  let x2 = width / 3, y2 = 950;
-  let m2 = 350 + cos(frameCount * 4) * 50;
-
-  push();
-  translate(x2, y2);
-
-  push();
-  translate(0, 0);
-  //noFill();  strokeWeight(10);  stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
-  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(28); stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
-  ellipse(0, 0, m2, m2 / 2);
-  //noFill();  strokeWeight(5);  stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
-  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(5); stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
-  //ellipse(0, 0, m, m/2);
-  pop();
-
-  copy(this.get(),
-    d2, d2, width - d2 * 2, height - d2 * 2,
-    d2 - x2, d2 - y2 + 5, width - d2 * 2, height - d2 * 2
-  );
-
-  pop();
-
-  // effect();
-  // drawWindow();
-  noStroke();
-  fill(90);
-  rect(0, height - 84, width, height);
-  fill(50);
-  rect(0, height - 64, width, height);
-
-
-  //
-  //vessel 3
-  //
-
-  randomSeed(seed);
-
-  let d3 = int(width / 30 + 50);
-  let x3 = width / 1.75, y3 = 1200;
-  let m3 = 450 + cos(frameCount * 4) * 50;
-
-  push();
-  translate(x3, y3);
-
-  push();
-  translate(0, 0);
-  //noFill();  strokeWeight(10);  stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
-  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(28); stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
-  ellipse(0, 0, m3, m3 / 2);
-  //noFill();  strokeWeight(5);  stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
-  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(5); stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
-  //ellipse(0, 0, m, m/2);
-  pop();
-
-  copy(this.get(),
-    d3, d3, width - d3 * 2, height - d3 * 2,
-    d3 - x3, d3 - y3 + 5, width - d3 * 2, height - d3 * 2
-  );
-
-  pop();
-
-  // effect();
-  // drawWindow();
-  noStroke();
-  fill(90);
-  rect(0, height - 84, width, height);
-  fill(50);
-  rect(0, height - 64, width, height);
-
-  //
-  //vessel number 4
-  //
-
-  randomSeed(seed);
-
-  let d4 = int(width / 30 + 50);
-  let x4 = width / 1.35, y4 = 400;
-  let m4 = 250 + cos(frameCount * 4) * 50;
-
-  push();
-  translate(x4, y4);
-
-  push();
-  translate(0, 0);
-  //noFill();  strokeWeight(10);  stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
-  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(28); stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
-  ellipse(0, 0, m4, m4 / 2);
-  //noFill();  strokeWeight(5);  stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
-  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(5); stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
-  //ellipse(0, 0, m, m/2);
-  pop();
-
-  copy(this.get(),
-    d4, d4, width - d3 * 2, height - d4 * 2,
-    d4 - x4, d4 - y4 + 5, width - d4 * 2, height - d4 * 2
-  );
-
-  pop();
-
-  //effect();
-  // drawWindow();
-  noStroke();
-  fill(90);
-  rect(0, height - 84, width, height);
-  fill(50);
-  rect(0, height - 64, width, height);
-
-  //
-  //vessel number 5
-  //
-
-  randomSeed(seed);
-
-  let d5 = int(width / 30 + 50);
-  let x5 = width / 4.5, y5 = 250;
-  let m5 = 300 + cos(frameCount * 5) * 50;
-
-  push();
-  translate(x5, y5);
-
-  push();
-  translate(0, 0);
-  //noFill();  strokeWeight(10);  stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
-  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(10); stroke(lerpColor(pal[2], pal[3], map(sin(frameCount), -1, 1, 0, 1)));
-  ellipse(0, 0, m5, m5 / 2);
-  //noFill();  strokeWeight(5);  stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
-  fill(lerpColor(pal[0], pal[1], map(cos(frameCount), -1, 1, 0, 1))); strokeWeight(5); stroke(lerpColor(pal[0], pal[1], map(sin(frameCount), -1, 1, 0, 1)));
-  //ellipse(0, 0, m, m/2);
-  pop();
-
-  copy(this.get(),
-    d5, d5, width - d5 * 2, height - d5 * 2,
-    d5 - x5, d5 - y5 + 5, width - d5 * 2, height - d5 * 2
-  );
-
-  pop();
-
-  //effect();
-  // drawWindow();
-  noStroke();
-  fill(90);
-  rect(0, height - 84, width, height);
-  fill(50);
-  rect(0, height - 64, width, height);
-
+  //noStroke();
+  //fill(90);
+  //rect(0, height - 84, width, height);
+  //fill(50);
+  //rect(0, height - 64, width, height);
 }
-
-// function keyPressed() {
-//     save('pix.jpg');
-// }
 
 function effect() {
   strokeWeight(1);
